85104008edc3cff186c8957ef68c0621f85265c4
Nicolas THAIZE <nicolas.thaize@ynov.com>
2021-05-30 00:29:44 +0200

    pagination component

---
+++ src/components/adminComponents/Pagination.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {UserContext} from "../../context";
+import CrudButtons from "./CrudButtons";
+
+class Pagination extends React.Component{
+  static contextType = UserContext;
+
+  // Props explanation:
+  // fields: The fields that will be showned (by object key, ex: {['id','title','author']})
+  // array: The array that will be processed into pagination (ex: [{},{},{}])
+  // crud: The array that will be processed to know which crud button needs to be rendered and what it does when pressed
+  // (if method specified: button exists, ex: {{show: this.showFunc, modify: this.mofifyFunc, delete: this.deleteFunc}})
+  // Full example of usage: <Pagination array={articles} fields={['id','title','author']} crud={{show: this.showFunc}}/>
+
+
+  state = {
+    objects: this.props.array,
+    numberOfPages: 0,
+    currentPage: 1,
+    shownObjects: [],
+    fields: this.props.fields
+  }
+
+  componentDidMount() {
+    let current = [], maxIndex = 5;
+    if (this.state.objects.length < 5) { // To avoid undefined index
+      maxIndex = this.state.objects.length
+    }
+    for (let index = 0; index < maxIndex; index++){
+      current.push(this.state.objects[index])
+    }
+
+    this.setState({
+      shownObjects: current,
+      numberOfPages: Math.round(this.state.objects.length / 5)
+    })
+    }
+
+  setArticlePages = (page = 1) => {
+    let current = [], maxIndex = page * 5;
+    if (this.state.objects[page*5] === undefined){
+      maxIndex = this.state.objects.length - 1;
+    }
+    let index = page * 5 - 5;
+    for (index; index < maxIndex; index++){
+      current.push(this.state.objects[index])
+    }
+    this.setState({shownObjects: current})
+  }
+
+  render() {
+    const { shownObjects, fields, numberOfPages } = this.state;
+    const pages = new Array(numberOfPages).fill(0);
+    return (
+      <div>
+        {shownObjects.map(object => (
+          <div key={object.id}>
+            { fields.map(field => (
+              <p key={object.id + field}>{field}: {object[field]}</p>
+            ))}
+            <CrudButtons
+              showFunc={this.props.crud.show ? () => this.props.crud.show(object) : undefined}
+              modifyFunc={this.props.crud.modify ? () => this.props.crud.modify(object) : undefined}
+              deleteFunc={this.props.crud.delete ? () => this.props.crud.delete(object) : undefined}
+            />
+          </div>
+        ))}
+
+        {pages.map((page, index) => (
+          <button key={index} onClick={() => this.setArticlePages(index+1)}>{index + 1}</button>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Pagination;
