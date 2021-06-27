b9557aa35312933d78b2693919b4b3fba0aa941f
Nicolas THAIZE <nicolas.thaize@ynov.com>
2021-06-27 17:24:19 +0200

    updating image service

--- src/ImageService.js
+++ src/ImageService.js
@@ -1,15 +1,20 @@
 import axiosInstance from "./axiosApi";
 
-
 class ImageService {
-  static async uploadImage(formData, name, alt){
+  static async uploadImage(formData, name, alt) {
     let result;
-    await axiosInstance.post('/images/', formData, {headers: {"Content-Type": "multipart/form-data"}}).then(async r => {
-      await axiosInstance.patch(`/images/${r.data.id}/`, {name: name, alt: alt}).then(r => {
-        result = r.data;
+    await axiosInstance
+      .post("/images/", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
       })
-    })
-    return result
+      .then(async (r) => {
+        await axiosInstance
+          .patch(`/images/${r.data.id}/`, { name: name, alt: alt })
+          .then((r) => {
+            result = r.data;
+          });
+      });
+    return result;
   }
 }
 
